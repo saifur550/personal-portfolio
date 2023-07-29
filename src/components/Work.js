c1e91320caf552c1aa44efc0fc9f196c122dd9a8
saifur550 <srstap550@gmail.com>
2023-07-29 18:11:54 +0600

    project item

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,32 +1,59 @@
 import React from 'react';
 import { motion } from 'framer-motion'
 import { fadeIn } from '../variants'
-import Image1 from '../assets/service-1.png'
+import Image1 from '../assets/work1.png'
 import Image2 from '../assets/service-2.png'
 import Image3 from '../assets/service-3.png'
 
 const Work = () => {
   return (
-    <section id='work' className='section'>
-      <div className='container mx-auto'>
-        <div>
-          <div>
-              <div>
-                <h2 className="h2 leading-tight text-accent">My Latest <br/> Work </h2>
-                <p className='max-w-sm mb-16'>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio, exercitationem necessitatibus? Velit quam quo exercitationem ad, dolores placeat, similique quibusdam neque optio, harum officiis et sed labore sequi eligendi cupiditate?
-                </p>
-                <button className="btn btn-sm">View All Project</button>
+    <section id='work' className='section mt-15'>
+      <div className="container mx-auto">
+          <div className="flex flex-col lg:flex-row gap-x-10">
+            <div className="flex-1 flex flex-col  gap-y-10    mb-10 lg:mb-0">
+              <div className="">
+              <h2 className="h2 leading-tight text-accent">My Latest Project</h2>
+              <p className="max-w-sm mb-16">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur culpa itaque voluptas, veritatis ut odit dolorem voluptatum enim aliquid hic.</p>
+              <button className="btn btn-sm">View All Work</button>
               </div>
-              <div>img</div>
+              <div className="  group relative overflow-hidden border-2 border-white/50 rounded-xl">
+                <div className=" group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+                <img src={Image1} alt="" className="  imgItem group-hover:scale-125 transition-all duration-500" />
+                <div className='absolute bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+                <span className="text-gradient">Digital Marketing</span>
+                </div>
+                <div className='absolute bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+                <span className="text-3xl text-white">Google Ads Words</span>
+               </div>
+                </div>
+            </div>
+            <div className="flex-1 flex flex-col gap-y-10">
+
+            <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
+            <div className=" group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+            <img src={Image1} alt="" className="imgItem  group-hover:scale-125 transition-all duration-500" />
+            <div className='absolute bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+            <span className="text-gradient">Digital Marketing</span>
+            </div>
+            <div className='absolute bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+            <span className="text-3xl text-white">Google Ads Words</span>
+           </div>
+            </div>
+
+            <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
+            <div className=" group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+            <img src={Image1} alt="" className="imgItem  group-hover:scale-125 transition-all duration-500" />
+            <div className='absolute bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+            <span className="text-gradient">Digital Marketing</span>
+            </div>
+            <div className='absolute bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+            <span className="text-3xl text-white">Google Ads Words</span>
+           </div>
+            </div>
+            
+            </div> 
           </div>
-        
-          <div>
-              <div></div>
-          </div>
-        </div>
       </div>
-    
     </section>
   );
 };
--- src/demo.js
+++ src/demo.js
@@ -1,70 +1,27 @@
-import React from 'react'
-import CountUp from 'react-countup'
-import { useInView } from 'react-intersection-observer'
-import { motion } from 'framer-motion'
-import { fadeIn } from '../variants'
-
-const About = () => {
-  const [ref, inView] = useInView({
-    threshold: 0.5
-  })
-
-  return (
-    <section ref={ref} id='about' className='section'>
-      <div className='container mx-auto'>
-        <div className='flex flex-col lg:flex-row gap-y-10 lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen'>
-           <div className='img flex-1 bg-about bg-contain bg-no-repeat  mix-blend-lighten bg-top h-[640px]'></div>
-          <div className='text  div-text '>
-            <h2 className='h2 text-cyan-400 '>About Me</h2>
-            <h3 className=''>
-              I'm a Freelance Front-end Developer & Digital marketing with over
-              3 years of Experience.
-            </h3>
-            <p className='mb-10'>
-              I'm saifur, a professional freelancer with extensive experience in
-              digital marketing who focuses on Google AdWords, Google Analytics
-              (GA4), Google Tag Manager, and Web Analytics. I have a
-              certification in "Digital Marketer," which includes specific
-              certifications in "Google PPC Ads," "Display Ads," "Shopping,"
-              "Leads," "Calls," "Apps," "Measurement," "Video," and "Analytics."{' '}
-            </p>
-            <div className='flex gap-x-6 lg:gap-x-10 mb-12'>
-              <div className=''>
-                <div className='text-[40px] font-tertiary text-gradient mb-2 '>
-                  { inView ? <CountUp start={0} end={3} duration={3} /> : null} 
-                </div>
-                <div className='font-primary text-sm tracking-[2px]'>
-                  Years of <br /> experience
-                </div>
-              </div>
-              <div className=''>
-                <div className='text-[40px] font-tertiary text-gradient mb-2 '>
-                { inView ? <CountUp start={0} end={100} duration={3} /> : null}
-                </div>
-                <div className='font-primary text-sm tracking-[2px]'>
-                  projects  <br /> completed
-                </div>
-              </div>
-              <div className=''>
-                <div className='text-[40px] font-tertiary text-gradient mb-2 '>
-                { inView ? <CountUp start={0} end={80} duration={3} /> : null}
-                </div>
-                <div className='font-primary text-sm tracking-[2px]'>
-                  Satisfied <br /> Clients
-                </div>
-              </div>
-            </div>
-            <div className='flex gap-x-8 items-center'>
-            <button className='btn fw2 font-bold btn-sm'>Contact Me</button>
-            <a href ="#" className='text-gradient btn-link'>
-            My Protfolio
-            </a>
-            </div>
-          </div>
+<section id='work' className='section'>
+<div className='container mx-auto'>
+  <div>
+    <div className='flex-1 flex flex-col gap-y-12 mb-10'>
+        <div>
+          <h2 className="h2 leading-tight text-accent">My Latest <br/> Work </h2>
+          <p className='max-w-sm mb-16'>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio, exercitationem necessitatibus? Velit quam quo exercitationem ad, dolores placeat, similique quibusdam neque optio, harum officiis et sed labore sequi eligendi cupiditate?
+          </p>
+          <button className="btn btn-sm">View All Project</button>
         </div>
-      </div>
-    </section>
-  )
-}
+        <div>img</div>
+    </div>
+  
+    <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+        <div className='ground-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-75'></div>
+        <img src={Image} alt="" />
+        <div>Digital Marketing</div>
+        <div>
+         <span className="">Google Ads Words</span>
+        </div>
+    </div>
+    <div className="">2</div>
+  </div>
+</div>
 
-export default About
+</section>
